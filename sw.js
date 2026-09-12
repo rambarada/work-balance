c53fec9e77a5b97a84d7c14bdd4ff5d0cfb56ed6
Ram Barada <rambarada123@gmail.com>
2026-09-12 10:55:52 +0300

    Show Android notifications through a service worker after one minute.

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', function (e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('notificationclick', function (e) {
+  e.notification.close();
+  e.waitUntil(
+    self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function (clientsArr) {
+      if (clientsArr.length) return clientsArr[0].focus();
+      return self.clients.openWindow('./');
+    })
+  );
+});
